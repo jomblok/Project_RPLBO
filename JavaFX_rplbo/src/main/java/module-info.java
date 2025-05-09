efa2167659fdc9eed85d3bdc3fcf48b66cf683e4
imelrantetandung <143603676+imelrantetandung@users.noreply.github.com>
2025-05-09 22:56:09 +0700

    Delete JavaFX_rplbo directory

--- JavaFX_rplbo/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module id.ac.ukdw.www.rpblo.javafx_rplbo {
-    requires javafx.controls;
-    requires javafx.fxml;  // Menambahkan modul javafx.fxml
-    requires javafx.base;
-    requires java.desktop;
-
-    opens id.ac.ukdw.www.rpblo.javafx_rplbo to javafx.fxml;  // Membuka paket agar dapat digunakan oleh javafx.fxml
-    exports id.ac.ukdw.www.rpblo.javafx_rplbo;
-}
